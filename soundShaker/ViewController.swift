87855e77cd7c513c30845bc30d5e68812698b956
Yisen Jing <jyisen@hotmail.com>
2016-04-12 20:37:29 -0700

    sound shake IOS Application made by Yuxi

--- soundShaker/ViewController.swift
+++ soundShaker/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["03","1a1a1","bach1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        
+        if event.subtype == UIEventSubtype.MotionShake {
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(sounds.count - 1)))
+            
+            
+            var filelocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            var error : NSError? = nil
+            
+            player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: filelocation!), error: &error)
+            
+            player.play()
+            
+            
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
